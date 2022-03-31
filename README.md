abee550d6cdcef303ffa3f68e6ff5df3a0b4ada4
rivus <sekreroy7@gmail.com>
2022-03-31 18:24:28 +0700

    now the program is terminal based and folders are customizable (you cannot unsort files yet)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# File Sorter
-<p>Sorting tool that sorts by file extension and puts it in folders</p>
-<b>TODO:</b>
-<ol>
-  <li>Make customizable (make console ver. use of console args)</li>
-  <li>Make GUI</li>
-</ol>
+# Simple File Extension Sorter
+<p>Sorting tool that sorts by file extension and puts it in folders (that are named by extension)</p>
+<b>Usage:</b>
+<p>-p (prefix) - sets prefixes for folders like '(prefix)txt' or '(prefix)doc'</p>
+<p>-n - create folders named by just extensions</p>
+<p>-h - shows help message</p>
+<p><del>-c - unsort, bring files back from folders (also deleting folders)</del> doesnt work yet</p>
---
+++ sfes.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from getopt import *
+from colorama import init
+from termcolor import colored
+
+def sort(prefix):
+	files = os.listdir()
+	extensions = list()
+	for file in files:
+		if "." in file and os.path.basename(__file__) != file and os.path.isfile(file) and file != "desktop.ini":
+			extensions.append(file.split(".")[-1])
+
+	extensions = list(dict.fromkeys(extensions))
+	for ext in extensions:
+		try:
+			os.mkdir(f"{prefix}{ext}")
+		except:
+			pass
+	for file in files:
+		for ext in extensions:
+			if file.split(".")[-1] == ext:
+				try:
+					os.rename(file, f"{prefix}{ext}/{file}")
+				except Exception as e:
+					print(colored(f"Failed {file} to move: {e}", "red"))
+	print(colored("Done!", "yellow"))
+	with open("cfg.sfes", "w") as file:
+		file.write(prefix)
+	print(colored("Do not delete cfg.sfes file if you will need to unsort your files!", "yellow"))
+	exit()
+
+
+def unsort(prefix):
+	print("in development")
+	exit()
+
+
+def help():
+	print(colored("Usage:\n-p (prefix) - sets prefixes for folders like '(prefix)txt' or '(prefix)doc'\n-n - create folders named by just extensions\n-h - shows this message\n-c - unsort, bring files back from folders (also deleting folders)", "yellow"))
+	exit()
+
+
+def main():
+	print("Simple File Extension Sorter. Made by Rivus (Utoog) 2022\n")
+	if len(sys.argv) == 1:
+		help()
+	try:
+		optlist, args = getopt(sys.argv[1:], 'np:hc')
+	except GetoptError as e:
+		print(colored("Error: " + str(e) + ", use 'sfes -h' for help", "red"))
+		return False
+
+	for opt, a in optlist:
+		if opt == "-h":
+			help()
+		elif opt == "-n":
+			yn = input("Are you sure that you want to have no prefix? You wont be able to unsort your files using sfes in the future!\n(yes or no? y/n): ")
+			if yn.lower() == "y":
+				sort("")
+			elif yn.lower() == "n":
+				exit()
+			else:
+				print("I`ll take that as no")
+				exit()
+		elif opt == "-p":
+			if a not in "+=[]:;«,./\\:*? «<>|":
+				yn = input("Proceed? (yes or no? y/n):")
+				if yn.lower() == "y":
+					sort(a)
+				elif yn.lower() == "n":
+					exit()
+				else:
+					print("I`ll take that as no")
+					exit()
+			else:
+				print(colored("Error: Can't create folders with this prefix!", "red"))
+				exit()
+		elif opt == "-c":
+			unsort(a)
+
+	
+
+
+if __name__ == "__main__":
+	main()
