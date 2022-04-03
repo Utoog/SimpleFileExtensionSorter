f508bca6bbec29a95fb9d56718f2cd56152f0c5c
rivus <sekreroy7@gmail.com>
2022-04-03 17:22:11 +0700

    version 1.1 with resort and unsort features

--- sfes.py
+++ sfes.py
@@ -27,26 +27,50 @@ def sort(prefix):
 	print(colored("Done!", "yellow"))
 	with open("cfg.sfes", "w") as file:
 		file.write(prefix)
-	print(colored("Do not delete cfg.sfes file if you will need to unsort your files!", "yellow"))
+	print(colored("Do not delete or edit cfg.sfes file if you will need to unsort your files!", "yellow"))
 	sys.exit()
 
 
 def unsort(prefix):
-	print("this function is currently in development")
+	try:
+		with open("cfg.sfes") as file:
+			prefix = file.readlines()[0]
+	except FileNotFoundError:
+		print(colored("Error: cfg.sfes not found, cannot resort", "red"))
+		sys.exit()
+	files = os.listdir()
+	folders = list()
+	extensions = list()
+	for file in files:
+		if prefix in file and os.path.isdir(file):
+			folders.append(file)
+			extensions.append(file.split(".")[-1])
+	for folder in folders:
+		files = os.listdir(folder)
+		for file in files:
+			try:
+				os.rename(f"{folder}/{file}", f"{file}")
+			except Exception as e:
+				print(colored(f"Failed {file} to move: {e}", "red"))
+		try:
+			os.rmdir(folder)
+		except Exception as e:
+			print(colored(f"Failed {file} to delete: {e}", "red"))
+	print(colored("Done!", "yellow"))
 	sys.exit()
 
 
 def help():
-	print(colored("Usage:\n-p (prefix) - sets prefixes for folders like '(prefix)txt' or '(prefix)doc'\n-n - create folders named by just extensions\n-h - shows this message\n-c - unsort, bring files back from folders (also deleting folders)", "yellow"))
+	print(colored("Usage:\n-p (prefix) - sets prefixes for folders like '(prefix)txt' or '(prefix)doc'\n-n - create folders named by just extensions\n-r - sorts unsorted files in existant folders using previous prefix\n-h - shows this message\n-c - unsort, bring files back from folders (also deleting folders)", "yellow"))
 	sys.exit()
 
 
 def main():
-	print("Simple File Extension Sorter. Made by Rivus (Utoog) 2022 v1.0\n")
+	print("Simple File Extension Sorter. Made by Rivus (Utoog) 2022 v1.1\n")
 	if len(sys.argv) == 1:
 		help()
 	try:
-		optlist, args = getopt(sys.argv[1:], 'np:hc')
+		optlist, args = getopt(sys.argv[1:], 'np:hcr')
 	except GetoptError as e:
 		print(colored("Error: " + str(e) + ", use 'sfes -h' for help", "red"))
 		sys.exit()
@@ -65,7 +89,7 @@ def main():
 				sys.exit()
 		elif opt == "-p":
 			if a not in "+=[]:;«,./\\:*? «<>|":
-				yn = input("Proceed? (yes or no? y/n):")
+				yn = input("Proceed? (yes or no? y/n): ")
 				if yn.lower() == "y":
 					sort(a)
 				elif yn.lower() == "n":
@@ -78,6 +102,20 @@ def main():
 				sys.exit()
 		elif opt == "-c":
 			unsort(a)
+		elif opt == "-r":
+			try:
+				yn = input("Proceed? (yes or no? y/n): ")
+				if yn.lower() == "y":
+					with open("cfg.sfes") as file:
+						sort(file.readlines()[0])
+				elif yn.lower() == "n":
+					sys.exit()
+				else:
+					print("I`ll take that as no")
+					sys.exit()
+			except FileNotFoundError:
+				print(colored("Error: cfg.sfes not found, cannot resort", "red"))
+				sys.exit()
 
 	
 
