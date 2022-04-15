f15b5f4330c857b2a9d2c0938f16dd62a6d95189
rivus <sekreroy7@gmail.com>
2022-04-15 20:02:22 +0700

    v1.12

--- sfes.py
+++ sfes.py
@@ -1,8 +1,38 @@
 import os
 import sys
 from getopt import *
-from colorama import init
-from termcolor import colored
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def colored(text, color):
+	match color:
+		case "header":
+			return bcolors.HEADER+text+bcolors.ENDC
+		case "okblue":
+			return bcolors.OKBLUE+text+bcolors.ENDC
+		case "okcyan":
+			return bcolors.OKCYAN+text+bcolors.ENDC
+		case "okgreen":
+			return bcolors.OKGREEN+text+bcolors.ENDC
+		case "warning":
+			return bcolors.WARNING+text+bcolors.ENDC
+		case "fail":
+			return bcolors.FAIL+text+bcolors.ENDC
+		case "bold":
+			return bcolors.BOLD+text+bcolors.ENDC
+		case "underline":
+			return bcolors.UNDERLINE+text+bcolors.ENDC
+		case _:
+			return text
 
 def sort(prefix):
 	files = os.listdir()
@@ -23,11 +53,11 @@ def sort(prefix):
 				try:
 					os.rename(file, f"{prefix}{ext}/{file}")
 				except Exception as e:
-					print(colored(f"Failed {file} to move: {e}", "red"))
-	print(colored("Done!", "yellow"))
+					print(colored(f"Failed {file} to move: {e}", "fail"))
+	print(colored("Done!", "okgreen"))
 	with open("cfg.sfes", "w") as file:
 		file.write(prefix)
-	print(colored("Do not delete or edit cfg.sfes file if you will need to unsort your files!", "yellow"))
+	print(colored("Do not delete or edit cfg.sfes file if you will need to unsort your files!", "warning"))
 	sys.exit()
 
 
@@ -36,7 +66,7 @@ def unsort(prefix):
 		with open("cfg.sfes") as file:
 			prefix = file.readlines()[0]
 	except FileNotFoundError:
-		print(colored("Error: cfg.sfes not found, cannot resort", "red"))
+		print(colored("Error: cfg.sfes not found, cannot resort", "fail"))
 		sys.exit()
 	files = os.listdir()
 	folders = list()
@@ -51,35 +81,35 @@ def unsort(prefix):
 			try:
 				os.rename(f"{folder}/{file}", f"{file}")
 			except Exception as e:
-				print(colored(f"Failed {file} to move: {e}", "red"))
+				print(colored(f"Failed {file} to move: {e}", "fail"))
 		try:
 			os.rmdir(folder)
 		except Exception as e:
-			print(colored(f"Failed {file} to delete: {e}", "red"))
-	print(colored("Done!", "yellow"))
+			print(colored(f"Failed {file} to delete: {e}", "fail"))
+	print(colored("Done!", "okgreen"))
 	sys.exit()
 
 
 def help():
-	print(colored("Usage:\n-p (prefix) - sets prefixes for folders like '(prefix)txt' or '(prefix)doc'\n-n - create folders named by just extensions\n-r - sorts unsorted files in existant folders using previous prefix\n-h - shows this message\n-c - unsort, bring files back from folders (also deleting folders)", "yellow"))
+	print(colored("Usage:\n-p (prefix) - sets prefixes for folders like '(prefix)txt' or '(prefix)doc'\n-n - create folders named by just extensions\n-r - sorts unsorted files in existant folders using previous prefix\n-h - shows this message\n-c - unsort, bring files back from folders (also deleting folders)", "warning"))
 	sys.exit()
 
 
 def main():
-	print("Simple File Extension Sorter. Made by Rivus (Utoog) 2022 v1.1\n")
+	print(colored("Simple File Extension Sorter v1.12\n", "header"))
 	if len(sys.argv) == 1:
 		help()
 	try:
 		optlist, args = getopt(sys.argv[1:], 'np:hcr')
 	except GetoptError as e:
-		print(colored("Error: " + str(e) + ", use 'sfes -h' for help", "red"))
+		print(colored("Error: " + str(e) + ", use 'sfes -h' for help", "fail"))
 		sys.exit()
 
 	for opt, a in optlist:
 		if opt == "-h":
 			help()
 		elif opt == "-n":
-			yn = input("Are you sure that you want to have no prefix? You wont be able to unsort your files using sfes in the future!\n(yes or no? y/n): ")
+			yn = input(colored("Are you sure that you want to have no prefix? You wont be able to unsort your files using sfes in the future!\n(yes or no? y/n): ", "warning"))
 			if yn.lower() == "y":
 				sort("")
 			elif yn.lower() == "n":
@@ -89,32 +119,18 @@ def main():
 				sys.exit()
 		elif opt == "-p":
 			if a not in "+=[]:;«,./\\:*? «<>|":
-				yn = input("Proceed? (yes or no? y/n): ")
-				if yn.lower() == "y":
-					sort(a)
-				elif yn.lower() == "n":
-					sys.exit()
-				else:
-					print("I`ll take that as no")
-					sys.exit()
+				sort(a)
 			else:
-				print(colored("Error: Can't create folders with this prefix!", "red"))
+				print(colored("Error: Can't create folders with this prefix!", "fail"))
 				sys.exit()
 		elif opt == "-c":
 			unsort(a)
 		elif opt == "-r":
 			try:
-				yn = input("Proceed? (yes or no? y/n): ")
-				if yn.lower() == "y":
-					with open("cfg.sfes") as file:
-						sort(file.readlines()[0])
-				elif yn.lower() == "n":
-					sys.exit()
-				else:
-					print("I`ll take that as no")
-					sys.exit()
+				with open("cfg.sfes") as file:
+					sort(file.readlines()[0])
 			except FileNotFoundError:
-				print(colored("Error: cfg.sfes not found, cannot resort", "red"))
+				print(colored("Error: cfg.sfes not found, cannot resort", "fail"))
 				sys.exit()
 
 	
