4b7d816e84ad50235f140211bb3277a9a4151db4
rivus <sekreroy7@gmail.com>
2023-11-01 21:33:27 +0700

    created project FINAL

--- sfes/sfes.cpp
+++ sfes/sfes.cpp
@@ -1,6 +1,27 @@
-﻿#include <iostream>
+#include <filesystem>
+#include <iostream>
+#include <string>
 
-int main()
+void sort(char* prefix)
+{
+    std::cout << "This doesn't work yet, prefix is '" << prefix << "'" << std::endl;
+    std::string path = "./";
+
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        std::cout << entry.path() << std::endl;
+    }
+}
+
+int main(int argc, char** argv)
 {
     std::cout << "Welcome to Simple File Extension Sorter v2.0" << std::endl;
+    if (argc > 1) 
+    {
+        sort(argv[1]);
+    }
+    else {
+        std::cout << "argument error" << std::endl;
+        return -1;
+    }
 }
